217579dc658fc9e8d4989aabf65ebfb26375586c
nonzamo <nonzamomwende57@gmail.com>
2024-08-20 14:06:56 -0400

    Phase-2-react-hooks-jsx

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
+import React from 'react';
 
-function About() {
-  return <div>About</div>;
-}
+
+const About = () => {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>I am a passionate web developer with a love for creating dynamic and user-friendly websites.</p>
+      <img src="https://res.cloudinary.com/highereducation/images/f_auto,q_auto/g_face,c_fill,fl_lossy,q_auto:best,w_448,h_382/v1662131294/ComputerScience.org/GettyImages-1097995482_711ffff4/GettyImages-1097995482_711ffff4.jpg?_i=AA" alt="About me" />
+    </div>
+  );
+};
 
 export default About;
